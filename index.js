f18db8c48e72fc9d78d8c924d7eeb7c6e567ae55
anthonylopez18 <a.lopez18@gmail.com>
2023-01-18 21:05:21 -0800

    bugfix

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.post('/',jsonParser, async function (req, res) {
         try{
             console.log(req.body);
             date = new Date(req.body.year, req.body.month-1, req.body.day);
-            var reqLanguage = req.body.language;
-            switch(reqLanguage.toString().toUpperCase()){
+            
+            switch(req.body.language){
                 case "ENGLISH":
                     language = "e";
                     rValue = 1
