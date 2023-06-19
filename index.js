1cc27969b31292742b00892df6b5c318773f9b34
anthonylopez18 <a.lopez18@gmail.com>
2023-06-18 22:08:17 -0700

    loadSchedule

--- index.js
+++ index.js
@@ -295,6 +295,137 @@ app.post('/',jsonParser, async function (req, res) {
     }
     
  })
+ app.post('/loadSchedule',jsonParser, async function (req, res) {
+    var isScheduleDataCached = false;
+    var scheduleObj;
+    var assignmentObj;
+    
+    try{
+        //var body;
+        var date = new Date();
+        var language = "e";
+        var rValue = 1;
+        try{
+            console.log(String(req.body.language).toUpperCase());
+            date = new Date(req.body.year, req.body.month-1, req.body.day);
+            
+            switch(String(req.body.language).toUpperCase()){
+                case "ENGLISH":
+                    language = "e";
+                    rValue = 1
+                    break;
+                case "TAGALOG":
+                    language = "tg";
+                    rValue = 27
+                    break;
+                default:
+                    language = "e";
+                    rValue = 1
+                    break;
+            }
+            
+        }
+        catch (error){
+            res.send('UGh....' + error);
+        }
+        
+        console.log('year: ' + date.getFullYear());
+        var beginningDate = new Date(date.getFullYear(), 0,1);
+        var reqDate = date; //new Date(Date.now());
+        
+        var diff = reqDate - beginningDate;
+        var diffTime = Math.abs(reqDate - beginningDate);
+        var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        console.log('diff: ' + diffDays);
+        var weekNumber = Number.parseFloat((diffDays/7)+1).toFixed(0);
+        console.log(weekNumber);
+        const url = "https://wol.jw.org/en/wol/meetings/r"+rValue+"/lp-"+language+"/"+date.getFullYear()+"/"+weekNumber;
+        var hasAssignment = false;
+
+        try{
+            console.log('url: '+url);
+            const responseObj = [];
+            const response = await fetch(url)
+                .then(res => res.text())
+                .then(
+                    (result) => {
+                        const $ = cheerio.load(result);
+                        const openningSection = $('#section1').find('ul');
+                        const treasuresSection = $('#section2').find('.so');
+                        const gemDetails = $('#section2').find('.sw');
+                        const ministrySection = $('#section3').find('li');
+                        const livingSection = $('#section4').find('li');
+
+                        openningSection.each(function(ind, el){
+                            console.log($(el).text());
+                            responseObj.push({
+                                sectionName: 'openning',
+                                title: $(el).text().trim().replace('\n\n', ' ')
+                            });
+                        });
+                        var gems =' '; 
+                        gemDetails.each(function (ind, el){
+                            gems += $(el).text() + ' ';   
+                        });
+                        treasuresSection.each(function(ind, el){
+                            if($(el).text().includes('Espirituwal na Hiyas')){
+                                responseObj.push({
+                                    sectionName: 'treasures',
+                                    title: $(el).text() +gems
+                                });   
+                            }
+                            else{
+                                responseObj.push({
+                                    sectionName: 'treasures',
+                                    title: $(el).text()
+                                });   
+                            }
+                            
+                        });
+
+                        ministrySection.each(function(ind, el){
+                            console.log($(el).text());
+                            responseObj.push({
+                                sectionName: 'ministry',
+                                title: $(el).text().trim().replace('\n\n', ' ')
+                            });
+                        });
+
+                        livingSection.each(function(ind, el){
+                            console.log($(el).text());
+                            responseObj.push({
+                                sectionName: 'living',
+                                title: $(el).text().trim().replace('\n\n', ' ')
+                            });
+                        });
+                    }
+                );
+                console.log('caching schedule...'+ weekNumber);
+                const scheduleRef = db.collection('schedule').doc(weekNumber);
+                console.log('obj: ' + responseObj.toString());
+                await scheduleRef.set({
+                    info : responseObj
+                });
+                res.send({
+                    success:true,
+                    response: responseObj,
+                });
+    
+        }catch{
+    
+        }
+        
+        
+    }
+    catch (error){
+        res.send({
+            success:false,
+            response: error
+        });
+    }
+    
+ })
+
  const PORT = process.env.PORT || 3000
  const HOST = process.env.HOST 
 var server = app.listen(PORT, function () {
